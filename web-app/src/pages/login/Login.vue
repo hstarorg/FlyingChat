e21c0901c3f20aabc82b4bfbb4a79e9189266e69
Hstarorg <hm910705@163.com>
2018-01-20 00:04:23 +0800

    login page improve

--- web-app/src/pages/login/Login.vue
+++ web-app/src/pages/login/Login.vue
@@ -11,22 +11,24 @@
         <div class="pull-left left-section">
           <img src="https://avatars0.githubusercontent.com/u/4043284?s=460&v=4" alt="logo"> &nbsp;
         </div>
-        <div class="pull-left center-section">
-          <div class="login-input-group username-group">
-            <input type="text" class="name-input" placeholder="账号" v-model="user.userName">
+        <form @submit.prevent="handleLogin">
+          <div class="pull-left center-section">
+            <div class="login-input-group username-group">
+              <input type="text" class="name-input" placeholder="账号" v-model="user.userName">
+            </div>
+            <div class="login-input-group password-group">
+              <input type="password" class="pwd-input" placeholder="密码" v-model="user.password">
+            </div>
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" v-model="rememberMe"> 记住账户
+              </label>
+            </div>
+            <div class="">
+              <button class="btn btn-primary btn-block">登录</button>
+            </div>
           </div>
-          <div class="login-input-group password-group">
-            <input type="password" class="pwd-input" placeholder="密码" v-model="user.password">
-          </div>
-          <div class="checkbox">
-            <label>
-              <input type="checkbox" v-model="rememberMe"> 记住账户
-            </label>
-          </div>
-          <div class="">
-            <button class="btn btn-primary btn-block" @click="handleLogin">登录</button>
-          </div>
-        </div>
+        </form>
         <div class="pull-left right-cection">
           <router-link to="/register">注册账号</router-link>
           <br>
@@ -36,4 +38,5 @@
     </div>
   </div>
 </template>
-<script src="./Login.controller.js"></script>
+<script src="./Login.controller.js">
+</script>
