0c6a070bed3d5bcd63fd2f3faefaf3fa7b244eea
Hstarorg <hm910705@163.com>
2018-01-22 22:20:40 +0800

    api logic

--- backend-api/src/bizs/socketIOBiz.js
+++ backend-api/src/bizs/socketIOBiz.js
@@ -9,8 +9,27 @@ const handlePreflightRequest = (req, res) => {
   res.end();
 };
 
+const userMap = new global.Map();
+
+function handleMessage(io, socket, msg) {
+  if (!msg || !msg.type) {
+    return;
+  }
+  switch (msg.type) {
+    case 'SEND_MESSAGE':
+      console.log(msg.message);
+      socket.to('user_10000_10001').broadcast.emit('message', {
+        type: 'NEW_MESSAGE',
+        message: {
+          message: msg.message.message
+        }
+      });
+      break;
+  }
+}
+
 const initSocketIOBiz = () => {
-  io.use(async (socket, next) => {
+  global.io.use(async (socket, next) => {
     const token = socket.handshake.headers[config.tokenName];
     if (token) {
       const userInfo = await tokenStore.get(token);
@@ -21,10 +40,21 @@ const initSocketIOBiz = () => {
     }
     return next(new Error('401'));
   });
-  io.on('connection', socket => {
-    socket.emit('test', 'hello');
+  global.io.on('connection', socket => {
+    const user = socket.handshake.user;
+    socket.join('user_10000_10001');
+    // 如果存在，先踢掉
+    if (userMap.has(user.userId)) {
+      const oldSocket = userMap.get(user.userId);
+      oldSocket.emit('message', { type: 'FORCED_DISCONNECT', message: '您的账号在其他设备登录，您已被强制下线。' });
+      oldSocket.disconnect();
+    }
+    userMap.set(user.userId, socket);
+    socket.on('message', msg => {
+      handleMessage(global.io, socket, msg);
+    });
     socket.on('disconnect', function() {
-      console.log('user disconnected');
+      userMap.delete(user.userId);
     });
   });
 };
