6af58c10046270edadc708a36660892dea8dc04c
Jay.M.Hu <jay.m.hu@newegg.com>
2018-03-29 20:35:30 +0800

    Test component

---
+++ mobile-app/src/screens/message/home/components/SessionItem.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export class SessionItem extends React.PureComponent<{}> {
+  render() {
+    return (
+      <View>
+        <Text>ABCDE</Text>
+      </View>
+    );
+  }
+}
